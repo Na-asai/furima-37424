38f6c8cc5721b81958c501eacfec3a5fde1d1e26
Na-asai <b98911218@gmail.com>
2022-06-14 02:05:20 +0900

    Revert "ユーザー管理機能作成"
    This reverts commit 0bcab2b5ece707b286b12ef63176064eec3a31d2.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
   before_action :configure_permitted_parameters, if: :devise_controller?
-
 
 
 
@@ -20,11 +18,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth])
   end
-
 
 
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,14 +6,12 @@ def index
 end
 
 
-
 private
 
 def move_to_index
   unless user_signed_in?
     redirect_to action: :index
   end
-
 end
 
 end
