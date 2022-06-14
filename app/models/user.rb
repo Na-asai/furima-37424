727ffd4efa287a228af9f8b03d5d2a505bebb483
Na-asai <b98911218@gmail.com>
2022-06-14 23:17:55 +0900

    エラーコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,password_length: 6..128
 
          validates :nickname, presence: true
          validates :birth, presence: true
