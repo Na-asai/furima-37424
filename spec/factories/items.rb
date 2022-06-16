6087db7d4653519bdc6fb20088a47868e73bdfec
Na-asai <b98911218@gmail.com>
2022-06-17 03:52:49 +0900

    FactoryBot設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    image                       {Faker::Lorem.sentence}
+    item_name                   {Faker::Name.initials(number: 6)}
+    item_text                   {Faker::Name.initials(number: 6)}
+    item_category               {id: 2}
+    item_sale_status            {id: 2}
+    item_shipping_fee_status    {id: 2}
+    prefecture                  {id: 2}
+    item_scheduled_delivery     {id: 2}
+    item_price                  {'100000'}
   end
 end
