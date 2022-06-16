379806ff13d8c911e539a6e6f1cbad1237bfb1ca
Na-asai <b98911218@gmail.com>
2022-06-16 18:24:23 +0900

    ActiveStrage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,14 +7,20 @@ end
 
 
 def new
+  @item = Item.new(item_params)
 end
 
 private
 
-def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
   end
 end
 
-end
