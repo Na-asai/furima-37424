0a79c12bc20c0b2c29be03536e8391c795c2517f
Na-asai <b98911218@gmail.com>
2022-06-23 16:06:51 +0900

    テストコード作成中

---
+++ spec/factories/order_delivery_addresses.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :order_delivery_address do
+    
+    card_number         { '4242424242424242' }
+    card_exp_month      { '12' }
+    card_exp_month      { '30' }
+    card_cvc            { '123' }
+    postal_code         { '123-4567' }
+    prefecture_id       { 2 }
+    city                { '名古屋市' }
+    addresses           { '愛知県' }
+    building            { 'コーポhoge' }
+    token               {"tok_abcdefghijk00000000000000000"}
+    
+  end
+end
---
+++ spec/models/order_delivery_address_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe OrderDeliveryAddress, type: :model do
+  describe '購入情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_delivery_address = FactoryBot.build(:order_delivery_address, user_id: user.id, item_id: item.id)
+    end
+  end
+
+  describe '商品購入機能' do
+    context '商品を購入できるとき' do
+      it 'すべての必要なデータが存在すれば登録できる' do
+        expect(@order_delivery_address).to be_valid
+      end
+    end
+    context '商品が購入できないとき' do
+      it 'card_numberが空だと登録できない' do
+      end
+      it 'card_exp_monthが空では登録できない' do
+      end
+      it 'card_exp_monthが空では登録できない' do
+      end
+      it 'card_cvcが空だと登録できない' do
+      end
+      it 'postal_codeが空だと登録できない' do
+      end
+      it 'prefecture_idが空だと登録できない' do
+      end
+      it 'cityが空だと登録できない' do
+      end
+      it 'addressesが空だと登録できない' do
+      end
+      it 'tokenが生成されていないと登録できない' do
+      end
+      it 'ユーザーが紐付いていなければ購入できない' do
+      end
+      it 'itemが紐付いていなければ購入できない' do
+      end
+    end
+  end
+end
