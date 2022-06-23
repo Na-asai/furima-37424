2b1ba9d28c5c603a7e8c50d2dec1ae2e5454fdd8
Na-asai <b98911218@gmail.com>
2022-06-23 21:02:03 +0900

    公開キー書き換え

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_a639b922e7d2e994b4c9e568");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
