f00fdb53ba71e8486d74d546f2396f96d19485aa
Na-asai <b98911218@gmail.com>
2022-06-23 13:37:09 +0900

    テストコード準備

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :order do
-    
+    card_number                      { 4242424242424242 }
+    card_exp_month                   { 12 }
+    card_exp_year                    { 30 }
+    card_cvc                         { 123 }
+    postal_code                      { 123-4567 }
+    prefecture_id                    { 2 }
+    city                             { '名古屋市' }
+    addresses                        { '愛知県' }
+    building                         { 'コーポhoge' }
+
+    association :user
+    association :item
+
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order = FactoryBot.build(:order)
+  end
+
+  describe '商品購入機能' do
+    context '商品を購入できるとき' do
+      it 'すべての必要なデータが存在すれば登録できる' do
+        expect(@order).to be_valid
+      end
+    end
+    context '商品が購入できないとき' do
+      it 'imageが空だと登録できない' do
+
+      end
+      it 'item_nameが空では登録できない' do
+
+      end
+      it 'item_textが空では登録できない' do
+
+      end
+      it 'item_category_idが空だと登録できない' do
+
+      end
+      it 'item_category_idが---だと登録できない' do
+
+      end
+      it 'item_sale_status_idが空だと登録できない' do
+
+      end
+      it 'item_sale_status_idが---だと登録できない' do
+
+      end
+      it 'item_shipping_fee_status_idが空だと登録できない' do
+
+      end
+      it 'item_shipping_fee_status_idが---だと登録できない' do
+
+      end
+      it 'prefecture_idが空だと登録できない' do
+
+      end
+      it 'prefecture_idが---だと登録できない' do
+
+      end
+      it 'item_scheduled_delivery_idが空だと登録できない' do
+
+      end
+      it 'item_scheduled_delivery_idが---だと登録できない' do
+
+      end
+      it 'item_priceが空だと登録できない' do
+
+      end
+      it 'item_priceが299以下だと登録できない' do
+
+      end
+      it 'item_priceが10000000以上だと登録できない' do
+
+      end
+      it 'item_priceが全角だと登録できない' do
+
+      end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+
+      end
+    end
+  end
 end
+
