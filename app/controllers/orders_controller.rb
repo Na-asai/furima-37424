da728685eabdb6edf2062efc68435a2c91d2b756
Na-asai <b98911218@gmail.com>
2022-06-23 09:41:30 +0900

    購入画面への移動

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,23 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
 
   def index
+    @item = Item.find(params[:item_id])
+    @order_delivery_address = OrderDeliveryAddress.new
   end
-  
+
+  def create
+
+  end
+
+  private
+
+  def order_params
+    params.permit( :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id).merge(user_id: current_user.id)
+    #params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  #def address_params
+    #params.permit( :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(order_id: @order.id)
+  #end
 end
--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -2,4 +2,7 @@ class DeliveryAddress < ApplicationRecord
 
   belongs_to :order
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
 end
