85b27fe6fee7b0d40896771be5ab1cbed94959fc
Na-asai <b98911218@gmail.com>
2022-06-23 10:51:57 +0900

    購入データ保存機能の作成中

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,17 +7,20 @@ class OrdersController < ApplicationController
   end
 
   def create
-
+    @order_delivery_address = OrderDeliveryAddress.new(order_params)
+    if@order_delivery_address.valid?
+      @order_delivery_address.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
 
   def order_params
-    params.permit( :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id).merge(user_id: current_user.id)
-    #params.permit(:item_id).merge(user_id: current_user.id)
+    params.permit( :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id)
+          .merge(user_id: current_user.id)
   end
 
-  #def address_params
-    #params.permit( :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(order_id: @order.id)
-  #end
 end
--- app/models/order_delivery_address.rb
+++ app/models/order_delivery_address.rb
@@ -15,6 +15,7 @@ class OrderDeliveryAddress
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
 
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number,order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses,
+                   building: building, phone_number: phone_number,order_id: order.id)
   end
 end
